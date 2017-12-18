6eae492be565c83e3cf38f8f34c5a1273163cb38
KrisBaulina <baulina-131197@mail.ru>
2017-12-18 20:22:55 +0700

    РГР 3

--- index.php
+++ index.php
@@ -23,23 +23,22 @@
 		<?php
 		
 		if (isset($myDate)){
-			$month=$myDate -> format('m');
-			$year=$myDate -> format('Y');				
-			$myDate->setDate((int)$year,(int)$month,1);
-			$dayOfWeek=$myDate->format('D');
-			echo $dayOfWeek;
-			$i=1;
-			while ($dayOfWeek !== 'Sat' || $dayOfWeek !== 'Sun'){
+			$month = $myDate -> format('m');
+			$year = $myDate -> format('Y');				
+			$myDate -> setDate((int)$year,(int)$month,1);
+			$dayOfWeek = $myDate->format('D');
+			$i=1;			
+			while ($dayOfWeek !== 'Sat' && $dayOfWeek !== 'Sun'){
 				$myDate->setDate((int)$year,(int)$month-$i,1);
 				$i++;
-				$dayOfWeek=$myDate->format('D');
-			};
-			if ($dayOfWeek=='Sat'){
-					echo "Последний раз первое число выпало на выходной ". $myDate . ", это была " . $daysOfWeek[$dayOfWeek];
-				}
-				else {
-					echo "Последний раз первое число выпало на выходной ". $myDate . ", это было " . $daysOfWeek[$dayOfWeek];
-				}	
+				$dayOfWeek = $myDate -> format('D');				
+			};					
+			if ($dayOfWeek == 'Sun'){
+				echo "Последний раз первое число выпало на выходной " . $myDate->format('d.m.Y') . ", это было " . $daysOfWeek[$dayOfWeek];
+			}
+			elseif ($dayOfWeek == 'Sat'){
+				echo "Последний раз первое число выпало на выходной " . $myDate->format('d.m.Y') . ", это была " . $daysOfWeek[$dayOfWeek];
+			}	
 		}			
 		?>	
 	</body>
